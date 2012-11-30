6de70c4502de47b0deae9f3190695f8b3abf1ab2
Clay Freeman <admin@darkfission.net>
2012-11-30 17:16:24 -0600

    Added an event handler that will allow modules to register for events based on input from clients.

---
+++ includes/eventHandling.php
@@ -0,0 +1,35 @@
+<?php
+	class EventHandling {
+		private static $events;
+		
+		public static function registerForEvent($event, $class, $callback) {
+			self::$events[] = array($event, $class, $callback);
+		}
+		
+		public static function unregisterForEvent($event, $class, $callback) {
+			foreach (self::$events as $id => $entry) {
+				if (strtolower($entry[1]->name) == strtolower($class->name)) {
+					unset(self::$events[$id]);
+				}
+			}
+		}
+		
+		public static function unregisterModuleFromEvents($module) {
+			foreach (self::$events as $id => $entry) {
+				if (strtolower($entry[1]->name) == strtolower($module)) {
+					unset(self::$events[$id]);
+				}
+			}
+		}
+		
+		public static function triggerEvent($event, $params) {
+			foreach (self::$events as $event) {
+				if (strtolower($event[0]) == strtolower($event)) {
+					$module = $event[1];
+					$callback = $event[2];
+					$module->$callback($params);
+				}
+			}
+		}
+	}
+?>
