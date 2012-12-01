5d0fe23248665dfe5b41e85cc708ed6b7176f1af
Clay Freeman <admin@darkfission.net>
2012-11-30 19:02:31 -0600

    Added main loop.

--- main.php
+++ main.php
@@ -4,5 +4,14 @@
 	require_once("includes/socket.php");
 	require_once("includes/socketManagement.php");
 	
-	// Code here
+	while (true) {
+		foreach (SocketManagement::getSocketIDs() as $id) {
+			$socket = SocketManagment::getSocketByID($id);
+			$data = $socket->receiveData($id);
+			if ($data != false) {
+				EventHandling::triggerEvent("rawDataReceived", array($id, $data));
+			}
+			usleep(10000);
+		}
+	}
 ?>
